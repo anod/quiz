7c937f1d1d427aeba2f1c6b8b03111ebfc254c0a
Alexandr Gavrishev <algavris@microsoft.com>
2017-12-18 16:48:10 +0200

    Refactor of Parser.java

--- Parser.java
+++ Parser.java
@@ -1,42 +1,130 @@
+import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-/**
- * This class is thread safe.
- */
-public class Parser {
-  private File file;
-  public synchronized void setFile(File f) {
-    file = f;
-  }
-  public synchronized File getFile() {
-    return file;
-  }
-  public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
+
+interface CharacterFilter {
+  Boolean accepts(char character);
+
+  static final CharacterFilter NoFilter = new CharacterFilter() {
+    @Override
+    public Boolean accepts(char character) {
+      return true;
     }
-    return output;
+  };
+
+  static final CharacterFilter UnicodeFilter = new CharacterFilter() {
+    @Override
+    public Boolean accepts(char character) {
+      return character < 0x80;
+    }
+  };
+
+}
+
+interface Parser extends Closeable {
+  String retrieve();
+
+  static class InputStreamContent implements Parser {
+    private final CharacterFilter filter;
+    private final InputStream inputStream;
+
+    InputStreamContent(InputStream inputStream, CharacterFilter filter) {
+      this.inputStream = inputStream;
+      this.filter = filter;
+    }
+
+    InputStreamContent(File file, CharacterFilter filter) {
+      this(new FileInputStream(file), filter);
+    }
+
+    InputStreamContent(File file) {
+      this(new FileInputStream(file), CharacterFilter.NoFilter);
+    }
+
+    @Override
+    public String retrieve() {
+      StringBuilder output = new StringBuilder();
+      int data;
+      while ((data = i.read()) != -1) {
+        if (filter.accepts((char) data)) {
+          output.append((char) data);
+        }
+      }
+      return output.toString();
+    }
+
+    @Override
+    public void close() throws IOException {
+      this.inputStream.close();
+    }
   }
-  public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
-        output += (char) data;
+
+  static class ParserSynchronized implements Parser {
+    private final Parser parser;
+
+    ParserSynchronized(Parser parser) {
+      this.parser = parser;
+    }
+
+    @Override
+    public synchronized String retrieve() {
+      try (Parser p = this.parser) {
+        return p.retrieve();
       }
     }
-    return output;
+
+    @Override
+    public synchronized void close() {
+      this.parser.close();
+    }
   }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
+}
+
+interface Storage extends Closeable {
+  void save(String content);
+
+  static class OutputStreamStorage implements Storage {
+    private OutputStream outputStream;
+
+    OutputStreamStorage(OutputStream outputStream) {
+      this.outputStream = outputStream;
+    }
+
+    OutputStreamStorage(File file) {
+      this(new FileOutputStream(file));
+    }
+
+    @Override
+    public void save(String content) {
+      this.outputStream.write(this.content.getBytes());
+      this.outputStream.flush();
+    }
+
+    @Override
+    public void close() throws IOException {
+      this.outputStream.close();
+    }
+  }
+
+  static class StorageSynchronized implements Storage {
+    private final Storage storage;
+
+    StorageSynchronized(Storage storage) {
+      this.storage = storage;
+    }
+
+    @Override
+    public synchronized save(String content) {
+      try (Storage s = this.storage) {
+        s.save(content);
+      }
+    }
+
+    @Override
+    public synchronized void close() throws IOException {
+      this.storage.close();
     }
   }
 }
